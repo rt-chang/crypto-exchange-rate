65b314ad899683caf003e8e9980eea4986550c5c
Raymond Chang <rchang21@gmail.com>
2020-09-06 21:06:28 -0400

    Fix formatting

--- app.py
+++ app.py
@@ -15,87 +15,86 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
-	print("Request:")
-	print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-	res = fulfillRequest(req)
+    res = fulfillRequest(req)
 
-	res = json.dumps(res, indent=4)
+    res = json.dumps(res, indent=4)
 
-	r = make_response(res)
-	r.headers['Content-Type'] = 'application/json'
-	return r
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 
 def fulfillRequest(req):
-	if req.get("result").get("action") != "getExchangeRate":
-		return {}
+    if req.get("result").get("action") != "getExchangeRate":
+        return {}
 
-	baseUrl = "https://api.cryptonator.com/api/ticker/"
+    baseUrl = "https://api.cryptonator.com/api/ticker/"
 
-	currencies = getCurrencies(req)
-	if len(currencies) != 2:
-		return {}
-	reqUrl = baseUrl + "-".join(currencies)
+    currencies = getCurrencies(req)
+    if len(currencies) != 2:
+        return {}
+    reqUrl = baseUrl + "-".join(currencies)
 
-	result = urlopen(reqUrl).read()
-	data = json.loads(result)
-	res = formatWebhook(data)
-	return res
+    result = urlopen(reqUrl).read()
+    data = json.loads(result)
+    res = formatWebhook(data)
+    return res
 
 
 def getCurrencies(req):
-	result = req.get("result")
-	params = result.get("parameters")
+    result = req.get("result")
+    params = result.get("parameters")
 
-	currencies = []
-	base = params.get("currency-base")
-	if base is not None:
-		currencies.append(base)
-	target = params.get("currency-target")
-	if target is not None:
-		currencies.append(target)
-	return currencies
+    currencies = []
+    base = params.get("currency-base")
+    if base is not None:
+        currencies.append(base)
+    target = params.get("currency-target")
+    if target is not None:
+        currencies.append(target)
+    return currencies
 
 
 def formatWebhook(data):
-	requestSuccess = data.get("success")
-	if not requestSuccess:
-		return {}
+    requestSuccess = data.get("success")
+    if not requestSuccess:
+        return {}
 
-	ticker = data.get("ticker")
+    ticker = data.get("ticker")
 
-	base = ticker.get("base")
-	target = ticker.get("target")
-	price = ticker.get("price")
-	change = ticker.get("change")
-	timestamp = data.get("timestamp")
+    base = ticker.get("base")
+    target = ticker.get("target")
+    price = ticker.get("price")
+    change = ticker.get("change")
+    timestamp = data.get("timestamp")
 
-	if base is None or target is None or price is None or \
-		change is None or timestamp is None:
-		return {}
+    if base is None or target is None or price is None or \
+            change is None or timestamp is None:
+        return {}
 
-	speech = "1 " + base + " is trading for " + price \
-			+ " " + target
+    speech = "1 " + base + " is trading for " + price \
+        + " " + target
 
-	print("Response:")
-	print(speech)
+    print("Response:")
+    print(speech)
 
-	return {
-		"speech": speech,
-		"displayText": speech,
-		"data": data,
-		"contextOut": [],
-		"source": "crypto-exchange-rate"
-	}
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "data": data,
+        "contextOut": [],
+        "source": "crypto-exchange-rate"
+    }
 
 
 if __name__ == "__main__":
-	port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
 
-	print("Starting app on port " + str(port))
+    print("Starting app on port " + str(port))
 
-	app.run(debug=True, port=port, host='0.0.0.0')
-
+    app.run(debug=True, port=port, host='0.0.0.0')
--- test.py
+++ test.py
@@ -8,24 +8,25 @@ from app import app
 class TestApp(unittest.TestCase):
 
 	def test_app(self):
-		self.test_app = app.test_client()
+        self.test_app = app.test_client()
 
-		payload = {
-			"result": {
-				"source": "agent",
-				"resolvedQuery": "CAD to USD",
-				"action": "getExchangeRate",
-				"parameters": {
-					"currency-base": "CAD",
-					"currency-target": "USD"
-				}
-			}
-		}
-		headers = {'content-type': 'application/json'}
-		response = self.test_app.post('/webhook',
-									  headers=headers,
-									  data=json.dumps(payload))
-		self.assertEqual(response.status, "200 OK")
+        payload = {
+            "result": {
+                "source": "agent",
+                "resolvedQuery": "CAD to USD",
+                "action": "getExchangeRate",
+                "parameters": {
+                    "currency-base": "CAD",
+                    "currency-target": "USD"
+                }
+            }
+        }
+        headers = {'content-type': 'application/json'}
+        response = self.test_app.post('/webhook',
+                                      headers=headers,
+                                      data=json.dumps(payload))
+        self.assertEqual(response.status, "200 OK")
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
